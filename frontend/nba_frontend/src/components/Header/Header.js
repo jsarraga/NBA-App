73e7940fc9dbfdbddcdd15d84b43e75e2969af97
Justin <jsarraga@gmail.com>
2019-10-17 14:20:59 -0400

    day1 header

--- frontend/nba_frontend/src/components/Header/Header.js
+++ frontend/nba_frontend/src/components/Header/Header.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import { Route } from 'react-router';
 import { Link } from 'react-router-dom';
+import Home from '../Home/Home';
+import Players from '../Players/players';
+import Team from '../Team/team';
+import Draft from '../Draft/Draft';
+import Comparison from '../Comparison/comparison';
+import Logo from './img/MJ.jpg';
 import './Header.css';
 
 
 function Header() {
+
     return(
         <div className="Header">
-            <img>Header logo/image</img>
+            <img src={Logo} alt="logo" width="65%"/> <br/>
             <topnav className="nav"> 
-                <Link type="nav" to="/Home">Home</Link>
-                <Link type="nav" to="/Players">Players</Link>
-                <Link type="nav" to="/Team">My Team</Link>
-                <Link type="nav" to="/Draft">Draft Team</Link>
-                <Link type="nav" to="/Comparison">Compare Players</Link>
+                <Link type="nav" className="link" to="/Home">Home</Link>
+                <Link type="nav" className="link" to="/Players">Players</Link>
+                <Link type="nav" className="link" to="/Team">My Team</Link>
+                <Link type="nav" className="link" to="/Draft">Draft Team</Link>
+                <Link type="nav" className="link" to="/Comparison">Compare Players</Link>
             </topnav>
                 <Route path='/Root' component={Home} />
                 <Route path='/Players' component={Players} />
