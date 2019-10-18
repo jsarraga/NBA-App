6417ea686ed3a81b21beb08177515281db5eec35
Justin <jsarraga@gmail.com>
2019-10-18 15:45:49 -0400

    loader done. need to account for multiple seasons. reorg schema

--- data/loader.py
+++ data/loader.py
@@ -1,35 +1,51 @@
 import csv 
 import sqlite3
-import os
-
-DIR = os.path.dirname(__file__)
-DBFILENAME = "nba.db"
-DBPATH = os.path.join(DIR, DBFILENAME)
 
 def file_reader(filename):
     with open(filename, 'r') as f:
         reader = csv.DictReader(f, delimiter=",")
         data = []
         for row in reader:
+            player_list = []
+            stat_dict = {}
             name = row["Player"].split("\\")
             row["Player"] = name[0]
-            data.append(row)
-        return data
+            stat_dict["name"] = row["Player"]
+            stat_dict["pos"] = row["Pos"]
+            stat_dict["pts"] = row["PTS"]
+            stat_dict["tpm"] = row["3P"]
+            stat_dict["reb"] = row["TRB"]
+            stat_dict["ast"] = row["AST"]
+            stat_dict["stl"] = row["STL"]
+            stat_dict["blk"] = row["BLK"]
+            stat_dict["fgp"] = row["FG%"]
+            stat_dict["ftp"] = row["FT%"]
+            stat_dict["tov"] = row["TOV"]
+            stat_dict["g"] = row["G"]
+            stat_dict["mp"] = row["MP"]
+            player_list.append(stat_dict)
+            data.append(player_list)
+        return(data)
+
+data1 = file_reader("raw_data/2018-2019.csv")
 
 
-file_reader('raw_data/2018-2019.csv')
 
 
+def load():
+    with sqlite3.connect("nba.db") as connection:
+        cur = connection.cursor()
 
+        data = data1
 
+        SQL = """ INSERT INTO players (name, pos, pts, tpm, reb, ast, 
+                    stl, blk, fgp, ftp, tov, g, mp) VALUES(?,?,?,
+                    ?,?,?,?,?,?,?,?,?,?); """
 
-#         sublist = []
-#         sublist.append(row)
-#         data.append(sublist)
+        for item in data:
+            for j in item:
+                cur.execute(SQL, (j["name"], j["pos"], j["pts"], j["tpm"],
+                j["reb"], j["ast"], j["stl"], j["blk"], j["fgp"], j["ftp"],
+                j["tov"],j["g"], j["mp"]))
 
-
-# with open ('output.csv', 'w') as f:
-#     writer = csv.writer(f)
-#     for row in data:
-#         writer.writerow(row)
-        
+load()
